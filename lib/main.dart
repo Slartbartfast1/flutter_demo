4f29f562978b9034a2f4428a00570c04ff94f76e
Huang <15212068@bjtu.edu.cn>
2019-08-04 18:08:09 +0800

    row & expanded

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext contexr) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'flutter demo',
+      title:'11111',
       home: Scaffold(
         appBar: new AppBar(
-          title: new Text('demo')
+          title: new Text('Row')
         ),
-        body:  GridView(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 2.0,
-            crossAxisSpacing: 2.0,
-            childAspectRatio: 0.7,
-          ),
-          children: <Widget>[
-            new Image.network('https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2563780504.webp',fit:BoxFit.cover),
-            new Image.network('https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2563780504.webp',fit:BoxFit.cover),
-            new Image.network('https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2563780504.webp',fit:BoxFit.cover),
-            new Image.network('https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2563780504.webp',fit:BoxFit.cover),
-            new Image.network('https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2563780504.webp',fit:BoxFit.cover),
-            new Image.network('https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2563780504.webp',fit:BoxFit.cover),
-          ],
-        )
+        body: 
+        new Row(children: <Widget>[
+         Expanded(
+           child: new RaisedButton(
+            onPressed:(){},
+            color: Colors.redAccent,
+            child: new Text('click me!')
+          ),) ,
+         Expanded(
+           child: new RaisedButton(
+            onPressed:(){},
+            color: Colors.redAccent[400],
+            child: new Text('click me!')
+          ),), 
+         Expanded(
+           child: new RaisedButton(
+            onPressed:(){},
+            color: Colors.redAccent[700],
+            child: new Text('click me!')
+          ),) 
+        ],),
       )
-    );
-  }
-}
-
-class MyList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      scrollDirection: Axis.horizontal,
-      children: < Widget > [
-        new Container(
-          width: 200.0,
-          color: Colors.lightBlue,
-        ),
-        new Container(
-          width: 200.0,
-          color: Colors.amber,
-        ),
-        new Container(
-          width: 200.0,
-          color: Colors.deepOrange,
-        ),
-        new Container(
-          width: 200.0,
-          color: Colors.purple,
-        ),
-      ],
     );
   }
 }
